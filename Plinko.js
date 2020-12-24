e654dae64c9592e185a7efe3a0f0f71b598142c3
prarthana-18 <73019866+prarthana-18@users.noreply.github.com>
2020-12-24 21:30:51 +0530

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,30 @@
+class Plinko{
+    constructor(x,y,radius){
+        var options ={
+            restitution:1,
+            friction:0,
+            isStatic:true
+        }
+        this.radius=10;
+        this.body=Bodies.circle(x,y,this.radius,options);
+      
+        World.add(world,this.body);
+
+    }
+    display(){
+        var pos = this.body.position;
+        var angle =this.body.angle;
+
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        fill(this.color);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.radius,this.radius);
+        pop();
+
+        
+    }
+}
